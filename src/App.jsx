6aedca12592addf81432ff1f3b3d43ffdca70b10
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-10-17 17:35:00 +0300

    test 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import PropType from "prop-types"
 import Painting from "./components/Painting";
 import data from "./components/data";
+
 
 
 export default function App() {
     return (
         <>
+            {[1, 2, 3, 4, 5,].map((el, index) => (
+                <div key={index}>{el}</div>))}
+
+            {data.map(data => (<Painting
+                key={data.id}
+                url={data.url}
+                title={data.title}
+                price={data.price}
+                tag={data.author.tag}
+                name={data.author.name}
+                profileUrl={data.author.url}
+            />))}
+
             {/* <Painting
                 url={data[0].url}
                 title={data[0].title}
@@ -12,30 +27,14 @@ export default function App() {
                 tag={data[0].author.tag}
                 name={data[0].author.name}
                 profileUrl={data[0].author.url}
-            />
-            <Painting
-                url={data[1].url}
-                title={data[1].title}
-                price={data[1].price}
-                tag={data[1].author.tag}
-                name={data[1].author.name}
-                profileUrl={data[1].author.url}
-            /> */}
-            {/* {data.map(data => {
-                <Painting
-                    key={data.key}
-                    url={data.url}
-                    title={data.title}
-                    price={data.price}
-                    tag={data.author.tag}
-                    name={data.author.name}
-                    profileUrl={data.author.url}
-                />
-            })} */}
-            <Painting
-                data={data} />
+            />*/}
+
+            {/* <Painting
+                data={data} /> */}
         </>
     );
 };
 
-// export default 5;
+data.PropType = {
+
+}
--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,44 +1,44 @@
-
-
-// const Painting = function ({ url = 'Q11', title, name, profileUrl, price, }) {
-//     return (
-//         <div>
-//             <img src={url} alt={title} width="480" />
-//             <h2>{title}</h2>
-//             <p>
-//                 Автор: <a href={profileUrl}>{name}</a>
-//             </p>
-//             <p>Цена: {price} грн</p>
-//             <p>Описание</p>
-//             <button type="button">Добавить в корзину</button>
-//         </div>
-//     );
-// };
-
 import imgError from "./Error.jpg";
 
-const Painting = function ({ data }) {
-
+const Painting = function ({ id, url = imgError, title, name, profileUrl, price, }) {
     return (
-        <ul>
-            {data.map(item => {
-                const { id, url = imgError, title, price, author: { name: name }, author: { url: profileUrl, } } = item;
-
-                return (
-                    <li key={id}>
-                        <img src={url} alt={title} width="480" />
-                        <h2>{title}</h2>
-                        <p>
-                            Автор: <a href={profileUrl}>{name}</a>
-                        </p>
-                        <p>Цена: {price} грн</p>
-                        <p>Описание</p>
-                        <button type="button">Добавить в корзину</button>
-                    </li>
-                )
-            })}
-        </ul>
+        <div key={id}>
+            <img src={url} alt={title} width="480" />
+            <h2>{title}</h2>
+            <p>
+                Автор: <a href={profileUrl}>{name}</a>
+            </p>
+            <p>Цена: {price} грн</p>
+            <p>Описание</p>
+            <button type="button">Добавить в корзину</button>
+        </div>
     );
 };
+
+
+
+// const Painting = function ({ data }) {
+
+//     return (
+//         <ul>
+//             {data.map(item => {
+//                 const { id, url = imgError, title, price, author: { name: name }, author: { url: profileUrl, } } = item;
+
+//                 return (
+//                     <li key={id}>
+//                         <img src={url} alt={title} width="480" />
+//                         <h2>{title}</h2>
+//                         <p>
+//                             Автор: <a href={profileUrl}>{name}</a>
+//                         </p>
+//                         <p>Цена: {price} грн</p>
+//                         <p>Описание</p>
+//                         <button type="button">Добавить в корзину</button>
+//                     </li>
+//                 )
+//             })}
+//         </ul>
+//     );
+// };
 
 export default Painting;
